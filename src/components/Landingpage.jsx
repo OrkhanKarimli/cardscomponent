d5f8b60b8ca15c2c1eb7ef94891fa6d0b3299eff
OrkhanKarimli <orkhansk@code.edu.az>
2023-04-26 15:24:00 +0400

    Finis Landing Page

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -4,11 +4,13 @@ import { ReactComponent as Donwarrow } from "../icons/Icon.svg";
 import { ReactComponent as Searchicon } from "../icons/Search.svg";
 import { ReactComponent as Caretdown } from "../icons/CaretDown.svg";
 import { ReactComponent as Logoicon } from "../icons/Logo.svg";
+import { ReactComponent as Patern } from "../icons/Patern.svg";
+
 const Landingpage = () => {
   return (
     <div className="container-fluid">
       <div className="row justify-content-center">
-        <div className="col-md-10 landingnav">
+        <div className="col-md-11 landingnav">
           <div className="row">
             <div className="col-md-7">
               <ul>
@@ -32,10 +34,31 @@ const Landingpage = () => {
                 </li>
               </ul>
             </div>
-            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <ul>
+                <li>
+                  <i>
+                    <Searchicon/>
+                  </i>
+                </li>
+                <li>
+                  <button>
+                    Sign In
+                  </button>
+                </li>
+                <li>
+                <button>
+                    Sign Up
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="col-md-8 landing-left">
+          <i className="patternicon">
+            <Patern/>
+          </i>
           <ul>
             <li>
               <h1>
