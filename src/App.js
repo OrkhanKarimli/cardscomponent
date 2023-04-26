3f94f8c5a212a2c0ea67d161f75cf54eec3cb018
OrkhanKarimli <orkhansk@code.edu.az>
2023-04-26 17:24:30 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import "./scss/style.scss";
+import courseimg from "../src/image/image118.png"
 import Coursecard from "./components/Coursecard"
 function App() {
   return(
-<Coursecard/>
+<Coursecard imgSrc={courseimg} courseAuthor={"Orkhan"} courseLevel={"Beginner"} courseModules={"fdb"} coursePrice={"128$"} courseTime={"44min"} courseTitle={"test"}/>
   ) 
 }
 
--- src/components/Coursecard.jsx
+++ src/components/Coursecard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+
 import { ReactComponent as Staricon } from "../icons/Star.svg";
 import { ReactComponent as Clockicon } from "../icons/Clock.svg";
 import { ReactComponent as Stackicon } from "../icons/Stack.svg";
 import { ReactComponent as Beginer } from "../icons/Beginer.svg";
-const Coursecard = () => {
+const Coursecard = ({
+  imgSrc,
+  courseTitle,
+  courseAuthor,
+  coursePrice,
+  courseTime,
+  courseModules,
+  courseLevel
+}) => {
   return (
     <div
       className="container"
@@ -14,34 +23,35 @@ const Coursecard = () => {
         <div className="col-md-4 coursecard_main">
           <div className="row">
             <div className="coursecard_image">
-              
+              <img src={imgSrc}>
+              </img>
             </div>
             <div className="col-md-12">
               <ul>
                 <li>
-                  <p>UX Design Foundations</p>
+                  <p>{courseTitle}</p>
                 </li>
                 <li>
-                  <p className="course_dot">By Ramazan Yenici</p>
+                  <p className="course_dot">{courseAuthor}</p>
                
                   <i>
                     <Staricon />
                   </i>
                   <p className="cardcourse_comments">4.8 (122)</p>
-                  <h4>$128</h4>
+                  <h4>{coursePrice}</h4>
                 </li>
                 <li>
                <label>
               <i><Clockicon/></i>
-              <p>40min</p>
+              <p>{courseTime}</p>
                </label>
                <label>
                <i><Stackicon/></i>
-              <p>21 Modules</p>
+              <p>{courseModules}</p>
                </label>
                <label>
                <i><Beginer/></i>
-              <p>Beginer Level</p>
+              <p>{courseModules}</p>
                </label>
                 </li>
               </ul>
