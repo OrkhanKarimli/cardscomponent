70b5fc928422f71173afacfdae3afd706d61f4d2
OrkhanKarimli <orkhansk@code.edu.az>
2023-04-27 10:31:33 +0400

    Fixing img of coursecard

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import courseimg from "../src/image/image118.png"
 import Coursecard from "./components/Coursecard"
 function App() {
   return(
-<Coursecard imgSrc={courseimg} courseAuthor={"Orkhan"} courseLevel={"Beginner"} courseModules={"fdb"} coursePrice={"128$"} courseTime={"44min"} courseTitle={"test"}/>
+<>
+</>
   ) 
 }
 
