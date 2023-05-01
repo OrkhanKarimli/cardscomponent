b437bdc731c249eab240bbff3c198fab48bb8926
OrkhanKarimli <orkhansk@code.edu.az>
2023-05-01 12:25:55 +0400

    Finish Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { ReactComponent as Footerpattern } from "../icons/Paternfooter.svg";
+import { ReactComponent as Facebook } from "../icons/FacebookLogo.svg";
+import { ReactComponent as Twitter } from "../icons/TwitterLogo.svg";
+import { ReactComponent as Linkedin } from "../icons/LinkedinLogo.svg";
+import { ReactComponent as Insragram } from "../icons/InstagramLogo.svg";
 import { ReactComponent as Icon1 } from "../icons/1.svg";
 import { ReactComponent as Icon2 } from "../icons/2.svg";
 import { ReactComponent as Icon3 } from "../icons/3.svg";
 import { ReactComponent as Icon4 } from "../icons/4.svg";
 import { ReactComponent as Icon5 } from "../icons/5.svg";
-import { ReactComponent as Edfooter } from "../icons/Edfooter.svg";
 const Footer = () => {
   return (
     <div className="container-fluid footer_main">
       <i>
         <Footerpattern />
       </i>
-      <div className="row">
+      <div className="row justify-content-center">
         <div className="col-md-12 cta">
           <ul>
             <li>
@@ -53,23 +56,101 @@ const Footer = () => {
           </ul>
         </div>
         <div className="col-md-12 footer_nav">
-          <div className="col-md-3">
-            <ul>
-              <li>
-                <i>
-                    <Edfooter/>
-                </i>
-              </li>
-              <li>
-                <p>
-                EdTech is a global online learning platform that offers anyone, anywhere access to online courses
-                </p>
-              </li>
-            </ul>
+          <div className="row">
+            <div className="col-md-5">
+              <ul>
+                <li>
+                  <div></div>
+                  <p>EdTech</p>
+                </li>
+
+                <li>
+                  <p>
+                    EdTech is a global online learning platform that <br />{" "}
+                    offers anyone, anywhere access to online courses
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-2">
+              <ul>
+                <li className="footer_nav_head">Pages</li>
+                <li>
+                  <p>Home</p>
+                </li>
+                <li>
+                  <p>Courses</p>
+                </li>
+                <li>
+                  <p>Events</p>
+                </li>
+                <li>
+                  <p>Authors</p>
+                </li>
+                <li>
+                  <p>About Us</p>
+                </li>
+                <li>
+                  <p>Teach on Edtech</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-2">
+              <ul>
+                <li className="footer_nav_head">Company</li>
+                <li>
+                  <p>Terms Conditions</p>
+                </li>
+                <li>
+                  <p>Privacy Policy</p>
+                </li>
+                <li>
+                  <p>Cookies</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-2">
+              <ul>
+                <li className="footer_nav_head">Community</li>
+                <li>
+                  <p>Help Center</p>
+                </li>
+                <li>
+                  <p>FAQ</p>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="col-md-3"></div>
-          <div className="col-md-3"></div>
-          <div className="col-md-3"></div>
+        </div>
+        <div className="col-md-10">
+          <div className="row">
+            <div className="col-md-4 copyright">
+              <p>©2022 EdTech. All rights reserved</p>
+            </div>
+            <div className="col-md-2 right-logos-footer">
+              <ul>
+                <li>
+                  
+                    <Facebook/>
+                  
+                </li>
+                <li>
+                  
+                    <Insragram/>
+                  
+                </li>
+                <li>
+                  
+                    <Twitter/>
+                  
+                </li>
+                <li>
+                  
+                  <Linkedin/>
+                  </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
