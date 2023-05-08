e403b1e63c553689ee1b2fe3d74d5b3289199257
OrkhanKarimli <orkhansk@code.edu.az>
2023-05-08 17:01:22 +0400

    Enough today

--- src/components/LandingPageCourses.jsx
+++ src/components/LandingPageCourses.jsx
@@ -1,46 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Collapse from "@mui/material/Collapse";
+import data from "../json/data.json";
+import {ReactComponent as Arrowclockwise} from "../icons/ArrowClockwise.svg";
+import { Input } from "@mui/material";
 const LandingPageCourses = () => {
-    const [checked, setChecked] = React.useState(true);
+  const [checked, setChecked] = React.useState(true);
+  const [isActive, setActive] = useState(null);
+  const toggle = (i) => {
+    if (isActive == i) {
+      return setActive(null);
+    }
+    else if(isActive!=true){
+        console.log("salam")
+    }
+    setActive(i);
+  };
+// const reset=()=>{
+//     setChecked(null)
+//     setActive(null)
+// }
+
   return (
     <div className="container-fluid landing-page-courses-main">
       <div className="col-md-2 landing-page-categories">
-        <ul className="landing-courses-list">
-          <h5>Categories</h5>
-          <li>
+        {data.map((item, i) => (
+          <ul key={i} className="item landing-courses-list">
             <input
               type="checkbox"
               defaultChecked={false}
               onChange={() => setChecked(!checked)}
             />
             <span className="customcheck"></span>
-            <span  className="listinfo">
-            UI/UX Design (160)
-            </span>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              defaultChecked={false}
-              onChange={() => setChecked(!checked)}
-            />
-            <span className="customcheck"></span>
-            <span  className="listinfo">
-            Graphic Design (94)
-            </span>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              defaultChecked={false}
-              onChange={() => setChecked(!checked)}
-            />
-            <span className="customcheck"></span>
-            <span className="listinfo">
-            3D & Animation (21)
-            </span>
-          </li>
-        </ul>
+            <div
+              className="landing-toggle-group head"
+              onClick={() => toggle(i)}
+            >
+              <p>{item.head}</p>
+              <span className="landing-browse-icon">
+                {isActive == i ? "-" : "+"}
+              </span>
+            </div>
+            <Collapse in={isActive == i ? true:false}>
+              {item.pages.map((index) => (
+                <p>{index.pagename}</p>
+              ))}
+            </Collapse>
+          </ul>
+        ))}
+        <span className="landing-browse-line"></span>
+
+        <div className="resetelements" >
+
+       <Arrowclockwise/> <p>
+       Reset Filter
+       </p>
+        </div>
       </div>
+
       <div className="col-md-9"></div>
     </div>
   );
