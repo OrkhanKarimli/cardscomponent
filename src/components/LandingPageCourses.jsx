7ac4101caa44e713ae1eceedefc6a6803632cb2a
OrkhanKarimli <orkhansk@code.edu.az>
2023-05-10 16:43:23 +0400

    Dynamic Accordion

--- src/components/LandingPageCourses.jsx
+++ src/components/LandingPageCourses.jsx
@@ -1,55 +1,15 @@
-import React, { useState } from "react";
-import Collapse from "@mui/material/Collapse";
-import data from "../json/data.json";
+import React from "react";
 import {ReactComponent as Arrowclockwise} from "../icons/ArrowClockwise.svg";
-import { Input } from "@mui/material";
+import data from "../json/data.json";
+import Multi from "../components/MultipleAccordion"
 const LandingPageCourses = () => {
-  const [checked, setChecked] = React.useState(true);
-  const [isActive, setActive] = useState(null);
-  const toggle = (i) => {
-    if (isActive == i) {
-      return setActive(null);
-    }
-    else if(isActive!=true){
-        console.log("salam")
-    }
-    setActive(i);
-  };
-// const reset=()=>{
-//     setChecked(null)
-//     setActive(null)
-// }
-
   return (
     <div className="container-fluid landing-page-courses-main">
       <div className="col-md-2 landing-page-categories">
-        {data.map((item, i) => (
-          <ul key={i} className="item landing-courses-list">
-            <input
-              type="checkbox"
-              defaultChecked={false}
-              onChange={() => setChecked(!checked)}
-            />
-            <span className="customcheck"></span>
-            <div
-              className="landing-toggle-group head"
-              onClick={() => toggle(i)}
-            >
-              <p>{item.head}</p>
-              <span className="landing-browse-icon">
-                {isActive == i ? "-" : "+"}
-              </span>
-            </div>
-            <Collapse in={isActive == i ? true:false}>
-              {item.pages.map((index) => (
-                <p>{index.pagename}</p>
-              ))}
-            </Collapse>
-          </ul>
-        ))}
+<Multi items={data} open={true}/>
         <span className="landing-browse-line"></span>
 
-        <div className="resetelements" >
+        <div className="resetelements">
 
        <Arrowclockwise/> <p>
        Reset Filter
---
+++ src/components/MultipleAccordion.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+const MultipleAccordion = ({ items, open, className }) => {
+  const [acardionItem, setAcardionitem] = useState(null);
+  useEffect(() => {
+    if (items) {
+      setAcardionitem([
+        ...items.map((item) => ({
+          ...item,
+          toggled: false,
+        })),
+      ]);
+    }
+  }, [items]);
+  function handleAccordionToggle(clickedItem) {
+    setAcardionitem([
+      ...acardionItem.map((item) => {
+        let toggled = item.toggled;
+        if (clickedItem.id == item.id) {
+          toggled = !item.toggled;
+        } else if (!open) {
+          toggled = false;
+        }
+        return {
+          ...item,
+          toggled,
+        };
+      }),
+    ]);
+  }
+  return (
+    <div className="landing-page-categories">
+      {acardionItem?.map((listItem, key) => {
+        return (
+          <>
+            <ul className="item landing-courses-list">
+              <input type="checkbox" defaultChecked={false} />
+              <span className="customcheck"></span>
+              <div
+                onClick={() => handleAccordionToggle(listItem)}
+                className="landing-toggle-group head "
+              >
+                <p>{listItem.head}</p>
+                <span className="landing-browse-icon">
+                  {listItem.toggled ? "-" : "+"}
+                </span>
+              </div>
+              <div
+                className={`toggle-content ${
+                  listItem.toggled ? "toggled" : ""
+                }`}
+                key={key}
+              >
+                {listItem.pages.map((index) => (
+                  <p>{index.pagename}</p>
+                ))}
+              </div>
+            </ul>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MultipleAccordion;
