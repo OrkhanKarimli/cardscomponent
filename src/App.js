0a77f6baf113233519fad69ac559f8764448bee8
OrkhanKarimli <orkhansk@code.edu.az>
2023-05-12 12:00:43 +0400

    Finish LearNer Dash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./scss/style.scss";
-import LandingPageCourses from "./components/LandingPageCourses";
+import LandingPageCourses from "./components/LearnerDash";
 function App() {
   return(
 <LandingPageCourses/>
--- src/components/LearnerDash.jsx
+++ src/components/LearnerDash.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ReactComponent as Searchicon } from "../icons/Search.svg";
+import { ReactComponent as Caretdown } from "../icons/CaretDown.svg";
+import userimg from "../image/learnerdashuser.png"
+import { ReactComponent as Logoicon } from "../icons/Logo.svg";
+import {ReactComponent as Bell} from "../icons/BellSimple.svg"
+const menu = [
+  {
+    head: "Dashboard ",
+  },
+  {
+    head: "Browse",
+  },
+  {
+    head: "My Course",
+  },
+  {
+    head: "My Schedule",
+  },
+  {
+    head: "Challenges"
+  }
+];
 
-const LearnerDash = () => {
+const LearnerDash = ({ learnerClass }) => {
+  const [isActive, setActive] = useState(null);
+  const toggle = (e) => {
+    setActive(e);
+  };
   return (
-    <div>LearnerDash</div>
+    <div className={`learnerdash ${learnerClass} col-md-12`}>
+    <div className="row">
+      <div className="col-md-7">
+        <ul>
+          <li>
+            <i>
+              <Logoicon />
+            </i>
+            <p className="DashLogoText">EdTech</p>
+          </li>
+          {menu.map((item, e) => (
+            <li onClick={() => toggle(e)} className={isActive == e ? "menustyledash" : ""}>
+              <pre>{item.head}</pre>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="col-md-4">
+        <ul>
+          <li>
+            <i>
+              <Searchicon />
+            </i>
+          </li>
+          <li>
+           <i>
+            <Bell/>
+           </i>
+          </li>
+          <li>
+           <div className="dashnavimg">
+            <img src={userimg} alt="" />
+           </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
   )
 }
 
